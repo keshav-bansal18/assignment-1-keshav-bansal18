497e5e481964f39052bab0d8919b0d4502091856
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-02-23 20:06:04 +0530

    add method

--- src/adt/SinglyADT.java
+++ src/adt/SinglyADT.java
@@ -1,5 +1,10 @@
 package adt;
 
 public interface SinglyADT<E> {
+    /*   This method will add the item in the list.
+        @param item the item to be added in the list.
+   */
+    void add(E item);
+
 
 }
