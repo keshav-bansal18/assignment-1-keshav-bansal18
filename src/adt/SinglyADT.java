8dcd450f14a105ef4b582fc6bbd950c0bf03c5f8
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-02-23 20:06:41 +0530

    remove method

--- src/adt/SinglyADT.java
+++ src/adt/SinglyADT.java
@@ -5,6 +5,10 @@ public interface SinglyADT<E> {
         @param item the item to be added in the list.
    */
     void add(E item);
+    //This method will remove an item from the linked list.
+
+    void remove();
+
 
 
 }
