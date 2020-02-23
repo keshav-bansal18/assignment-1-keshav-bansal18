5ad7f2e596b7542d2065383f66f951dbcb504d99
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-02-23 20:15:23 +0530

    create SinglyLinkedList class

---
+++ src/definition/SinglyLlnkedList.java
@@ -0,0 +1,31 @@
+package definition;
+
+import adt.SinglyADT;
+
+public class SinglyLlnkedList<E> implements SinglyADT<E> {
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
